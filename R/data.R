9d78ce7f00d31624435cb0375645cc8e53177806
Haley Jeppson <hjeppson@iastate.edu>
2017-03-30 16:48:02 -0500

    changed data format, added documentation

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Cousin Marriage Data
+#'
+#' A dataset containing the percentage of marriages that are consanguineous for various countries
+#'
+#' @format A data frame with 70 rows and 2 variables:
+#' \describe{
+#'   \item{Country}{country}
+#'   \item{Percent}{percentage of marriages that are consanguineous}
+#'   ...
+#' }
+#' @source \url{https://github.com/fivethirtyeight/data/tree/master/cousin-marriage}
+"cousin"

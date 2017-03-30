f6b1bb4db6dcd018ad4354b44dcbe5761462a08c
Amy Crawford <amy@amycrawfordsmbp.student.iastate.edu>
2017-03-30 16:51:44 -0500

    A little bit of work on the byContinent.R file

---
+++ R/byContinent.R
@@ -0,0 +1,11 @@
+#' Output a vector containing the continent of the country
+#'
+#' @param x A vector of country names.
+#' @export
+#' @return A vector of continent names.
+#' @examples
+#' byContinent()
+#' add(10, 1)
+add <- function(x, y) {
+  x + y
+}

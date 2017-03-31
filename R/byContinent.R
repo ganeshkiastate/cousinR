ca62e936079612913a7fd6db307df0f2960480ab
Haley Jeppson <hjeppson@iastate.edu>
2017-03-31 09:43:54 -0500

    updated function and documentation - need to fix dependencies to removes check() warnings

--- R/byContinent.R
+++ R/byContinent.R
@@ -1,14 +1,16 @@
 #' Output a vector containing the continent of the country
 #'
-#' @param x A data frame with two columns: country names and percentages.
+#' @param x A vector of country names and percentages.
+#' @param y A vector of percentages.
 #' @export
 #' @return A data frame with two columns: continent and average percentage.
 #' @examples
 #' data(cousin)
-#' byContinent(cousin)
+#' byContinent(cousin$Country, cousin$Percent)
 
 byContinent <- function(x, y){
   df <- data.frame(x, y)
-  x$continents <- countrycode(x, origin = "country.name", destination = "continent")
-  x %>% group_by(continents) %>% summarize(y)
+  df <- df %>% mutate(continent =  getFromNamespace("countrycode", asNamespace("countrycode"))(x, origin = "country.name", destination = "continent")) %>%
+    group_by(continent) %>% summarize(summary = mean(y))
+  return(df)
 }
